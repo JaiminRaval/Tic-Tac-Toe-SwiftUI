00e6a13c176cc4150ba6378e00fd3840c9519ffd
JaiminRaval <siliconvalley.jaimin@gmail.com>
2026-06-19 00:09:36 +0530

    Initial Commit

---
+++ Tic-Tac-Toe-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe-SwiftUI
+//
+//  Created by Jaimin Raval on 19/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic-Tac-Toe-SwiftUI/Tic_Tac_Toe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_SwiftUIApp.swift
+//  Tic-Tac-Toe-SwiftUI
+//
+//  Created by Jaimin Raval on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
